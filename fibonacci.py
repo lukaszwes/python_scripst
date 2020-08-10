84e3aef3f83db4a40f9625defbdb6dd882400024
Łukasz W <lukaszwes@gmail.com>
2020-08-10 17:00:46 +0200

    fibo update

--- fibonacci.py
+++ fibonacci.py
@@ -3,14 +3,11 @@
 x = int(input('Podaj ilosc elementow ciagu: '))
 
 def Fibonacci (numElements):
-    if numElements == 1:
-        fibo=[0]
-    elif numElements == 2:
-        fibo=[0,1]
-    elif numElements>2:
-        fibo=[0,1]
-        for x in range(3, numElements+1):
-            fibo.append(fibo[x-3]+fibo[x-2])
+    first, second = 0, 1
+    fibo = []
+    for _ in range(numElements):
+        first, second = second, first + second
+        fibo.append(first)
     return fibo
 
 
